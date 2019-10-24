8970f5595d33b8f6771c7a4742cdfb2e731b77b5
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-10-24 23:13:47 +0900

    add: reference

--- snippet/gcd_lcm.py
+++ snippet/gcd_lcm.py
@@ -1,3 +1,5 @@
+# reference: https://qiita.com/knakajima3027/items/b871631b8997a6d67223#%E6%9C%80%E5%A4%A7%E5%85%AC%E7%B4%84%E6%95%B0%E6%9C%80%E5%B0%8F%E5%85%AC%E5%80%8D%E6%95%B0
+
 """
 最大公約数
 Params:
