a60d7108a9f1d7f7d97c19f4156bd2d0792a4d38
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-10-24 23:30:12 +0900

    add: reference and example

--- snippet/divisor.py
+++ snippet/divisor.py
@@ -1,9 +1,17 @@
+# reference: https://qiita.com/knakajima3027/items/b871631b8997a6d67223#%E6%9C%80%E5%A4%A7%E5%85%AC%E7%B4%84%E6%95%B0%E6%9C%80%E5%B0%8F%E5%85%AC%E5%80%8D%E6%95%B0
+
 """
 約数の列挙
+
 Param:
     n: 約数を求めたい数
 Return:
     res: nの約数を列挙したlist
+
+Example:
+> res = divisor(12)
+> print(res)
+[1, 2, 3, 4, 6, 12]
 """
 def divisor(n:int):
     i = 1
