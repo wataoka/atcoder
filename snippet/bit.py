ffa98940e97818fb288c1cab156c0ab152b44849
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-10-26 22:05:12 +0900

    add: bit

---
+++ snippet/bit.py
@@ -0,0 +1,11 @@
+# reference: https://qiita.com/knakajima3027/items/b871631b8997a6d67223 
+
+import itertools
+
+L = [0, 1]
+num = 3
+bit_list = list(itertools.product(L, repeat=num))
+
+'''実行結果
+[(0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)]
+'''
