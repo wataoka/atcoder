135f4d00fd701c13a462b3b77031562e063dda8c
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-11-16 15:09:01 +0900

    refact: astar

--- snippet/astar.py
+++ snippet/astar.py
@@ -26,43 +26,39 @@ def heuristic(pos, goal):
     return max(abs(pos[0]-goal[0]), abs(pos[1]-goal[1]))
 
 # 累積コスト関数
-def distance(path):
+def cumulative_cost(path):
     return len(path)
 
 # 迷路をレンダリングする関数
 def render_path(path, maze):
     buf = [[ch for ch in l] for l in maze]
     for pos in path[1:-1]:
-        buf[pos[0]][pos[1]] = "*"
+        buf[pos[0]][pos[1]] = "o"
     buf[path[0][0]][path[0][1]] = "s"
     buf[path[-1][0]][path[-1][1]] = "g"
     return ["".join(l) for l in buf]
 
 
 # A*アルゴリズム
-def astar(start_pos, goal, maze):
-    closed_list = [start_pos]
-    start_score = distance(closed_list) + heuristic(start_pos, goal)
-    checked_dict = {start_pos: start_score}
-    searching_heap = []
-    heapq.heappush(searching_heap, (start_score, closed_list))
+def astar(start_pos, goal_pos, maze):
+    start_score = 0 + heuristic(start_pos, goal_pos)
 
-    while len(searching_heap) > 0:
-        score, closed_list = heapq.heappop(searching_heap)
-        last_passed_pos = closed_list[-1]
+    open_list = []
+    heapq.heappush(open_list, (start_score, [start_pos]))
+    score_dict = {start_pos: start_score}
 
-        if last_passed_pos == goal:
+    while len(open_list) > 0:
+        _, closed_list = heapq.heappop(open_list)
+        last_closed_pos = closed_list[-1]
+
+        if last_closed_pos == goal:
             return closed_list
-
-        for pos in nexts(last_passed_pos, maze):
-            new_closed_list = closed_list + [pos]
-            pos_score = distance(new_closed_list) + heuristic(pos, goal)
-
-            if (pos in checked_dict) and (checked_dict[pos] <= pos_score):
-                continue
-
-            checked_dict[pos] = pos_score
-            heapq.heappush(searching_heap, (pos_score, new_closed_list))
+        
+        for cur_pos in nexts(last_closed_pos, maze):
+            cur_pos_score = cumulative_cost(closed_list+[cur_pos]) + heuristic(cur_pos, goal_pos)
+            if not(cur_pos in score_dict) or cur_pos_score < score_dict[cur_pos]:
+                score_dict[cur_pos] = cur_pos_score
+                heapq.heappush(open_list, (cur_pos_score, closed_list+[cur_pos]))
 
     return []
 
