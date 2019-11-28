9a16f8fad335111e2c119a612a8be42f33b7085f
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-11-28 22:25:28 +0900

    add: comb mod in snippet

--- snippet/combination.py
+++ snippet/combination.py
@@ -33,3 +33,25 @@ def comb_num(a, b):
             result *= int(numerator[k])
 
     return result
+
+# comb_num_modがnCr modを返す
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+def comb_num_mod(n, r, mod=10**9+7):
+    r = min(r, n-r)
+    res = 1
+    for i in range(r):
+        res = res * (n - i) * modinv(i+1, mod) % mod
+    return res
