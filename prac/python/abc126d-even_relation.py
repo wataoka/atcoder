6aa38958915373d7ad8aa3f6593117e9d3ce8c6a
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-10 22:24:23 +0900

    add: abc126d

---
+++ prac/python/abc126d-even_relation.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+N = int(input())
+edge = defaultdict(list)
+# 辺の情報と距離を記録
+for i in range(N-1):
+    u, v, w = map(int, input().split())
+    edge[u].append((v, w))
+    edge[v].append((u, w))
+
+# 幅優先探索
+distance = [0]*N
+queue = [(1, -1)]
+while True:
+    if len(queue) == 0:
+        break
+    cur_id, parent_id = queue.pop()
+    for child_id, w in edge[cur_id].copy():
+        if child_id == parent_id:
+            continue
+        queue.append((child_id, cur_id))
+        distance[child_id-1] = distance[cur_id-1] + w
+
+for i in range(N):
+    print(distance[i]%2)
