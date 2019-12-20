836c94e18c684579ec629f9b8539edb4cd1991f8
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-20 21:55:37 +0900

    add: unifold

---
+++ snippet/unifold.py
@@ -0,0 +1,51 @@
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+        self.ans = n*(n-1)//2
+ 
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+ 
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+    
+        if x == y:
+            return
+ 
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        sx = self.size(x)
+        sy = self.size(y)
+        self.ans += sx*(sx-1)//2 + sy*(sy-1)//2
+        self.ans -= (sx+sy)*(sx+sy-1)//2
+ 
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+ 
+    def size(self, x):
+        return -self.parents[self.find(x)]
+ 
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+ 
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+ 
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+ 
+    def group_count(self):
+        return len(self.roots())
+ 
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
+ 
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
