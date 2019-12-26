9fd725da132e7407b17776b0b303f86e131d7804
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-26 20:13:50 +0900

    abc126c

--- abc126/c.py
+++ abc126/c.py
@@ -1,3 +1,14 @@
-n, k = list(map(int, input().split()))
+import math
 
-ans = 0.0
+n, k = map(int, input().split())
+ans = []
+for i in range(1, n+1):
+    wins = 0
+    score = i
+    while True:
+        if score >= k:
+            break
+        score *= 2
+        wins += 1
+    ans.append(pow(0.5, wins))
+print(sum(ans)/len(ans))
