612ee1ee9d8d68e24f03abec90f2dd881b632c3d
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-03-08 10:05:18 +0900

    add: e.py in abc158

--- abc158/e.py
+++ abc158/e.py
@@ -1,12 +1,34 @@
-N, P = map(int, input().split())
-S = input()
+import sys
+from collections import deque
 
-cnt = 0
-Px = 0
-while True:
-    Px += P
-    if Px > int(S):
-        break
-    if str(Px) in S:
-        cnt += S.count(str(Px))
-print(cnt)
+def input():
+    return sys.stdin.readline()[:-1]
+
+def solve():
+    N, P = map(int, input().split())
+    S = input()
+    if 10%P == 0:
+        ans = 0
+        for i in range(N):
+            if int(S[N-i-1]) % P == 0:
+                ans += N - i
+        
+        return ans
+    
+    ten = 1
+    cur = 0
+    val = [0] * P
+    val[cur] += 1
+    for i in range(N):
+        cur = (cur + int(S[N-i-1])*ten) % P
+        ten *= 10
+        ten %= P
+        val[cur] += 1
+    ans = 0
+    for p in range(P):
+        ans += val[p] * (val[p] - 1) // 2
+    
+    return ans
+
+if __name__ == "__main__":
+    print(solve())
